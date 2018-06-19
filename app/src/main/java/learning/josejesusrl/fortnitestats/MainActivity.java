5c50c57d320ad1f86b1adef50b2141dd207eb9ed
Jose Jesus Rios Lopez <josejesusrlopez@likson.com>
2018-06-19 16:28:03 -0500

    Seccion de codigo por funcion, ahora es mas legible

--- app/src/main/java/learning/josejesusrl/fortnitestats/MainActivity.java
+++ app/src/main/java/learning/josejesusrl/fortnitestats/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         snPlataform = (Spinner) findViewById(R.id.snPlataform);
         etPlayer = (EditText) findViewById(R.id.etNombre);
 
-        String[] gameModes = {"Solo", "Duo", "Squad", "En todos"};
+        String[] gameModes = {"Solo", "Duo", "Squad", "Todos"};
         String[] plataforms = {"PC", "PlayStation 4", "Xbox One"};
         arrayListView = new ArrayList<String>();
         arrayListView.add("Ingresa los datos para ver tus estadisticas de juego");
@@ -46,26 +46,70 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(listViewAdapter);
     }
 
-
-    // Obtenemos las estadicas llamando al objeto Stats que se encarga de depurar las estadisticas
-    public void getStats(View v){
-        NThread nt = new NThread(this, etPlayer.getText().toString(), snPlataform.getSelectedItem().toString(), snGamemode.getSelectedItem().toString());
+    private int getGamemode(){
+        switch (snGamemode.getSelectedItemPosition()){
+            case 0:
+                Log.d("MainAct.getGamemode", "Modo de juego seleccionado: "+NThread.SOLO);
+                return NThread.SOLO;
+            case 1:
+                Log.d("MainAct.getGamemode", "Modo de juego seleccionado: "+NThread.DUO);
+                return NThread.DUO;
+            case 2:
+                Log.d("MainAct.getGamemode", "Modo de juego seleccionado: "+NThread.SQUAD);
+                return NThread.SQUAD;
+            case 3:
+                Log.d("MainAct.getGamemode", "Modo de juego seleccionado: "+NThread.ALL);
+                return NThread.ALL;
+            default:
+                Log.d("MainAct.getGamemode", "Modo de juego seleccionado: "+-1);
+                return -1;
+        }
+    }
+    private int getPlataform(){
+        switch (snPlataform.getSelectedItemPosition()){
+            case 0:
+                Log.d("MainAct.getPlataform", "Plataforma seleccionada: "+NThread.PC);
+                return NThread.PC;
+            case 1:
+                Log.d("MainAct.getPlataform", "Plataforma seleccionada: "+NThread.PS4);
+                return NThread.PS4;
+            case 2:
+                Log.d("MainAct.getPlataform", "Plataforma seleccionada: "+NThread.XBOX);
+                return NThread.XBOX;
+            default:
+                Log.d("MainAct.getPlataform", "Plataforma seleccionada: "+-1);
+                return -1;
+        }
+    }
+    // Metodo para crear el objeto Nthread y regresar el mismo objeto ya con las stats
+    private NThread makeStats(){
+        NThread nt = new NThread(this, etPlayer.getText().toString(), getPlataform(), getGamemode());
         nt.getStats();
+
         Toast.makeText(this, "Un momento porfavor...", Toast.LENGTH_LONG);
-        // Obtenemos las estadisticas del jugador para manipularlo mas facil
-        GameStats gameStats = nt.getPlayerStats().getGameStats();
+
+        // Esperamos al servidor y al parse
         try {
-            // Esperamos al servidor y al parse
             Thread.sleep(3000);
         } catch(InterruptedException e) {}
 
+        // Retornamos el objeto Stats ya con las estadisticas del usuario
+        return nt;
+    }
 
-        if (nt.getPlayerStats() != null){
-            // Borramos el mensaje inicial
-            arrayListView.remove(0);
+    // Metodo para colocar las estadisticas en la GUI
+    private void putStats(){
+        NThread nt = makeStats();
+        Stats stats = nt.getPlayerStats();
+
+        if (stats != null){
+
+            GameStats gameStats = stats.getGameStats();
+
+
             // Ingresamos todos los datos de las estadisticas
-            arrayListView.add("Plataforma: "+nt.getPlataform());
-            arrayListView.add("Modo de juego: "+nt.getGameMode());
+            arrayListView.add("Usuario: "+nt.getUser());
+            arrayListView.add("Modo de jugo: "+nt.getGameMode());
             arrayListView.add("K/D ratio: "+gameStats.getKpd());
             arrayListView.add("Victorias: "+gameStats.getWins());
             arrayListView.add("Ratio de Victorias: "+gameStats.getWinRate());
@@ -75,12 +119,26 @@ public class MainActivity extends AppCompatActivity {
             // Regrescamos el ListView
             listViewAdapter.notifyDataSetChanged();
 
-            Log.i("MainActivity", "Las estats fueron añadidas");
-
-
+            Log.i("MainAct.putStats", "Las Stats fueron añadidas: "+stats.getId());
         }else{
             Log.w("MainActivity", "Los Stats son nullos");
         }
+    }
+
+    // Obtenemos las estadicas llamando al objeto Stats que se encarga de depurar las estadisticas
+    public void getStats(View v){
+        if (getPlataform() == -1){
+            Toast.makeText(this, "Porfavor selecciona una plataforma", Toast.LENGTH_SHORT);
+            Log.i("MainActivity.getStats()", "No se a seleccionado ninguna plataforma");
+        }
+        if (getGamemode() == -1){
+            Toast.makeText(this, "Porfavor selecciona un modo de juego", Toast.LENGTH_SHORT);
+            Log.i("MainActivity.getStats()", "No se a seleccionado ningun modo de juego");
+        }
+        if (getPlataform() != -1 && getGamemode() != -1){
+            putStats();
+        }
+
     }
 
     public void saveUserPreference(View v){
