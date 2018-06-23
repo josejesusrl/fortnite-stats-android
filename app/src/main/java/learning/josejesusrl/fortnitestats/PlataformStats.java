c91d3adc5e4cf49febd64c37f20b131a96747dbc
Jose Jesus Rios Lopez <josejesusrlopez@likson.com>
2018-06-23 18:49:32 -0500

    Añadimos objeto PlataformStats

---
+++ app/src/main/java/learning/josejesusrl/fortnitestats/PlataformStats.java
@@ -0,0 +1,42 @@
+package learning.josejesusrl.fortnitestats;
+
+public class PlataformStats {
+
+    private GameStats solo;
+    private GameStats duo;
+    private GameStats squad;
+    private GameStats all;
+
+    public GameStats getSolo() {
+        return solo;
+    }
+
+    public void setSolo(GameStats solo) {
+        this.solo = solo;
+    }
+
+    public GameStats getDuo() {
+        return duo;
+    }
+
+    public void setDuo(GameStats duo) {
+        this.duo = duo;
+    }
+
+    public GameStats getSquad() {
+        return squad;
+    }
+
+    public void setSquad(GameStats squad) {
+        this.squad = squad;
+    }
+
+    public GameStats getAll() {
+        return all;
+    }
+
+    public void setAll(GameStats all) {
+        this.all = all;
+    }
+
+}
