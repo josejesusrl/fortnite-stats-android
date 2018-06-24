70eb16becef25e12b5d79f36147489f5339c2ec7
Jose Jesus Rios Lopez <josejesusrlopez@likson.com>
2018-06-23 22:24:16 -0500

    Usando Gson como serailizador

--- app/src/main/java/learning/josejesusrl/fortnitestats/ApiConnection.java
+++ app/src/main/java/learning/josejesusrl/fortnitestats/ApiConnection.java
@@ -1,14 +1,15 @@
 package learning.josejesusrl.fortnitestats;
 
 import android.content.Context;
-import android.os.AsyncTask;
-import android.util.JsonReader;
-import android.util.JsonToken;
-import android.widget.Toast;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -17,17 +18,16 @@ import javax.net.ssl.HttpsURLConnection;
 public class ApiConnection {
 
     // Variable con la informacion del usuario
-    private String user;
-    private int plataform;
-    private Context context;
-    private String apiKey;
-    private int gameMode;
+    private String user, apiKey;
+    private int plataform, gameMode;
     private Stats stats;
 
     // Endpoints
     private String EP_stats;
 
     // Constantes
+
+    private static final String DATE_PATTERN = "yyyy-MM-dd'T'hh:mm:ss";
 
     // Modos de Juego
     public static final int SOLO = 10;
@@ -44,8 +44,6 @@ public class ApiConnection {
 
     // Constructor
     public ApiConnection(String user, int plataform, int gameMode) {
-        // Contexto de la app para enviar Toast
-        this.context = context;
 
         setUser(user);
         setPlataform(plataform);
@@ -53,16 +51,13 @@ public class ApiConnection {
 
         // Agregamos el API Key
         apiKey = "fyHRmMRjJg6FTgkbfASi";
-
         // Agregamos los EndPoints
         EP_stats = "https://fortnite.y3n.co/v2/player/";
     }
 
 
-
-
-
     // Getters
+
     // Metodo para obtener el nombre de la plataforma para el usuario final
     public String getPlataformName() {
         if (plataform == PC){
@@ -151,132 +146,22 @@ public class ApiConnection {
 
 
     // Creamos un reader para serealizar el JSON
-    private Stats readJsonStream(InputStream in) throws IOException {
-        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+    private Stats parseJson(InputStream in) throws IOException {
+        Reader reader = new InputStreamReader(in, "UTF-8");
         try {
-            return readStats(reader);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            StatsContainer statsContainer = gson.fromJson(reader, StatsContainer.class);
+            Log.d("ApiConn", "stats: "+statsContainer.toString());
+            return statsContainer.getStats();
         } finally {
             reader.close();
         }
     }
 
-    // Funcion Iniciar y Finalizar la Serializacion de JSON
-    private Stats readStats(JsonReader reader) throws IOException {
-        // En el objeto Stats se guardara toda la informacion del jugador
-        Stats stats = null;
-        while (reader.hasNext()) {
-            stats = readStatsUser(reader);
-        }
-        reader.endObject();
-        return stats;
-    }
-
-    // Iniciamos la serializacion del Json extrayendo los primeros datos
-    private Stats readStatsUser(JsonReader reader) throws IOException {
-        String id = "-1";
-        String lastUpdate;
-        String userID;
-        String diplayName;
-        String displayNameLowerCase;
-        GameStats gameStats = null;
-
-        // TODO crear una copia del reader para extraer datos posteriores a "br" en el json
-        JsonReader cReader = reader;
-
-        reader.beginObject(); // Ingresamos a la estrucutra inicial del JSON
-        while (reader.hasNext()) {
-            String name = reader.nextName();
-            if (name.equals("_id")) {
-                id = reader.nextString();
-            } else if (name.equals("br")) { // Estrucutura donde se encutra las stats del jugador
-                gameStats = readGameStats(reader);
-            } else {
-                reader.skipValue();
-            }
-        }
-
-        reader.endObject();
-        return new Stats(id, gameStats);
-    }
-
-    // Ingresamos a la estrucura donde estan los datos y vemos si no contiene nada
-    private GameStats readGameStats(JsonReader reader) throws IOException {
-        reader.beginObject(); // Entramos a la estrucutra br
-        while (reader.hasNext()) {
-            if (reader.nextName().equalsIgnoreCase("stats")) {
-                reader.beginObject(); // Entramos a Stats
-                if (reader.nextName().equalsIgnoreCase(getPlataform()) && reader.hasNext()) {
-                    reader.beginObject();// Entramos a la estructura de la plataforma a buscar
-
-                    // Buscamos la estructura  del modo de juego seleccionado
-                    while (reader.hasNext()) {
-
-                        if (reader.nextName().equalsIgnoreCase(getGameMode())) {
-                            return readPlayerStats(reader); // Enviamos la estructura a su serializacion
-                        } else {
-                            reader.skipValue();
-                        }
-                    }
-
-                } else {
-                    return null;
-                }
-            } else {
-                reader.skipValue();
-            }
-        }
-        return null;
-    }
-
-    // Buscamos y asginamos las estadisticas del usuario al objeto GameStats para retornarlo
-    private GameStats readPlayerStats(JsonReader reader) throws IOException {
-        int kills = 0, matchesPlayed = 0;
-        String lastMatch = null;
-        int minutesPlayed = 0, wins = 0, top10 = 0, top25 = 0, deaths = 0;
-        double kpd = 0, kpm = 0, tpm = 0, score = 0, winRate = 0;
-
-        reader.beginObject(); // Entramos a la estrucutura de la plataforma seleccionada
-        while (reader.hasNext()) {
-            String name = reader.nextName();
-            if (name.equalsIgnoreCase("kills")) {
-                kills = reader.nextInt();
-            } else if (name.equalsIgnoreCase("matchesPlayed") && reader.peek() != JsonToken.NULL) {
-                matchesPlayed = reader.nextInt();
-            } else if (name.equalsIgnoreCase("lastMatch") && reader.peek() != JsonToken.NULL) {
-                lastMatch = reader.nextString();
-            } else if (name.equalsIgnoreCase("minutesPlayed") && reader.peek() != JsonToken.NULL) {
-                minutesPlayed = reader.nextInt();
-            } else if (name.equalsIgnoreCase("wins") && reader.peek() != JsonToken.NULL) {
-                wins = reader.nextInt();
-            } else if (name.equalsIgnoreCase("top10") && reader.peek() != JsonToken.NULL) {
-                top10 = reader.nextInt();
-            } else if (name.equalsIgnoreCase("top25") && reader.peek() != JsonToken.NULL) {
-                top25 = reader.nextInt();
-            } else if (name.equalsIgnoreCase("deaths") && reader.peek() != JsonToken.NULL) {
-                deaths = reader.nextInt();
-            } else if (name.equalsIgnoreCase("kpd") && reader.peek() != JsonToken.NULL) {
-                kpd = reader.nextDouble();
-            } else if (name.equalsIgnoreCase("kpm") && reader.peek() != JsonToken.NULL) {
-                kpm = reader.nextDouble();
-            } else if (name.equalsIgnoreCase("tpm") && reader.peek() != JsonToken.NULL) {
-                tpm = reader.nextDouble();
-            } else if (name.equalsIgnoreCase("score") && reader.peek() != JsonToken.NULL) {
-                score = reader.nextDouble();
-            } else if (name.equalsIgnoreCase("winRate") && reader.peek() != JsonToken.NULL) {
-                winRate = reader.nextDouble();
-            } else {
-                reader.skipValue();
-            }
-        }
-        reader.endObject();
-        return new GameStats(kills, matchesPlayed, lastMatch, minutesPlayed, wins, top10, top25, deaths, kpd, kpm, tpm, score, winRate);
-    }
-
     // Funcion principal para obtener las estadisticas del jugador
-    public void getStats() {
+    public void downloadStats() {
 
         Stats estadisticas;
-        // Creaos la URL con el EndPoint de Fortnite stats
         URL fortniteStatsEndPoint = null;
 
 
@@ -292,23 +177,21 @@ public class ApiConnection {
             // Agregamos la Key del api de Fortnite Stats
             statsConnection.setRequestProperty("X-Key", apiKey);
 
-            // Recibiendo respuestas
-
             if (statsConnection.getResponseCode() == 200) {
                 // Recibimos los datros
                 InputStream responseBody = statsConnection.getInputStream();
 
                 // Enviamos a procesar los datos del Json y guaramos el objeto para ser consultado por otras clases
-                setStats(readJsonStream(responseBody));
+                setStats(parseJson(responseBody));
 
                 // Cerramos la conexion
                 statsConnection.disconnect();
 
 
             } else if (statsConnection.getResponseCode() == 503) {
-                System.out.println("Error 503");
+                Log.w("ApiConn", "Error 503 el servidor");
             } else if (statsConnection.getResponseCode() == 429) {
-                System.out.println("Error 429");
+                Log.w("ApiConn", "Error 429 el servidor");
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
